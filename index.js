8848b3756ee92a88a44f9b6e313c1726a8b9febf
Nicole0509 <niashavril0509@gmail.com>
2025-07-28 20:42:18 +0200

    js file updated and some styling

---
+++ index.js
@@ -0,0 +1,33 @@
+let scoreA = 0
+let scoreB = 0
+let
+
+function add1pointA(){
+    scoreA += 1
+    document.getElementById("scoreA").textContent =  scoreA
+}
+
+function add2pointA(){
+    scoreA += 2
+    document.getElementById("scoreA").textContent =  scoreA
+}
+
+function add3pointA(){
+    scoreA += 3
+    document.getElementById("scoreA").textContent =  scoreA
+}
+
+function add1pointB(){
+    scoreB += 1
+    document.getElementById("scoreB").textContent =  scoreB
+}
+
+function add2pointB(){
+    scoreB += 2
+    document.getElementById("scoreB").textContent =  scoreB
+}
+
+function add3pointB(){
+    scoreB += 3
+    document.getElementById("scoreB").textContent =  scoreB
+}
